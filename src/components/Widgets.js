1df5b65af73b16bd9d34aaf065aa55a578505222
RChaubey16 <ruturajchaubey16@gmail.com>
2022-01-15 16:58:21 +0530

    Added twitter embed library for live twitter updates

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import './Widgets.css';
+import React from "react";
+import "./Widgets.css";
+
+import SearchIcon from "@mui/icons-material/Search";
+import { Timeline, Tweet } from "react-twitter-widgets";
 
 function Widgets() {
-    return (
-        <div className="widgets">
-            <h2>Widgets</h2>
-        </div>
-    )
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twiiter" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening?</h2>
+        <Tweet tweetId="841418541026877441" />
+
+        <Timeline
+          dataSource={{
+            sourceType: "profile",
+            screenName: "elonmusk",
+          }}
+          options={{
+            height: "400",
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
